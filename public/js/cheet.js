92b6d7de4562f16bf006beb1c4a281bba3981719
Tiago Amaro <tiagocis@gmail.com>
2016-02-13 23:32:34 -0200

    other doom cheats should work as well :)

--- public/js/cheet.js
+++ public/js/cheet.js
@@ -168,3 +168,5 @@
 var show_marine = function() { document.getElementById('easter_egg').style.display = "inline-block"; };
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', show_marine);
 cheet('i d d q d', show_marine);
+cheet('i d k f a', show_marine);
+cheet('i d f a', show_marine);
