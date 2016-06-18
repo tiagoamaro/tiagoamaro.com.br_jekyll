3e241a9893b2ef47ef1ac4bdbf9af2cfa6f4d100
Tiago Amaro <tiagopadrela@gmail.com>
2016-06-18 16:56:37 +0200

    new empty theme file and deleting poole hyde theme

--- public/js/easter_egg.js
+++
@@ -1,7 +0,0 @@
-// Show Easter egg
-
-var show_marine = function() { document.getElementById('easter_egg').style.display = "inline-block"; };
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', show_marine);
-cheet('i d d q d', show_marine);
-cheet('i d k f a', show_marine);
-cheet('i d f a', show_marine);
