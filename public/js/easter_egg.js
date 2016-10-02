43531ce9a45db614861716e0eb44d325ae20dbe9
Tiago Amaro <tiagopadrela@gmail.com>
2016-10-02 12:53:32 +0200

    remove JS from head
    What I was thinking? =D

--- public/js/easter_egg.js
+++ public/js/easter_egg.js
@@ -1,9 +1,10 @@
 "use strict";
 
 // Show Easter egg
-
 var show_marine = function() { document.getElementById('easter_egg').style.display = "inline-block"; };
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', show_marine);
-cheet('i d d q d', show_marine);
-cheet('i d k f a', show_marine);
-cheet('i d f a', show_marine);
+document.addEventListener("DOMContentLoaded", function () {
+  cheet('↑ ↑ ↓ ↓ ← → ← → b a', show_marine);
+  cheet('i d d q d', show_marine);
+  cheet('i d k f a', show_marine);
+  cheet('i d f a', show_marine);
+});
--- public/js/mobile_menu.js
+++ public/js/mobile_menu.js
@@ -1,16 +1,16 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var menu_arrow      = document.querySelector(".mobile-menu-arrow");
-  var menu_arrow_icon = document.querySelector(".mobile-menu-arrow .fa-angle-down");
-  var sidebar_items   = document.querySelectorAll(".sidebar .sidebar-nav, .sidebar .sidebar-copyright, .sidebar .social-networks");
+  var menuArrow     = document.querySelector(".mobile-menu-arrow");
+  var menuArrowIcon = document.querySelector(".mobile-menu-arrow .fa-angle-down");
+  var sidebarItems  = document.querySelectorAll(".sidebar .sidebar-nav, .sidebar .sidebar-copyright, .sidebar .social-networks");
 
-  menu_arrow.onclick = function () {
-    menu_arrow_icon.classList.toggle("fa-angle-down");
-    menu_arrow_icon.classList.toggle("fa-angle-up");
+  menuArrow.onclick = function () {
+    menuArrowIcon.classList.toggle("fa-angle-down");
+    menuArrowIcon.classList.toggle("fa-angle-up");
 
-    for (var i = 0; i < sidebar_items.length; i++) {
-      sidebar_items[i].classList.toggle("show-mobile")
+    for (var i = 0; i < sidebarItems.length; i++) {
+      sidebarItems[i].classList.toggle("show-mobile")
     }
   }
 });
